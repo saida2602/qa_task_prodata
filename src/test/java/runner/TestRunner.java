1b44ab48ece79c6b85f1481d90c237c4dc060757
saida2602 <saidaaliyeva729@gmail.com>
2025-08-03 00:58:38 +0400

    deleted comments in TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,6 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-//        features = "src/test/resources/features/login.feature",
         features = "src/test/resources/features",
         glue = "stepdefinitions",
         plugin = {"pretty", "html:target/cucumber-report.html"},
